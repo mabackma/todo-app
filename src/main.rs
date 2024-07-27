92e5c0da2cb64c74b160e2c8d4579e52c953f2c5
Marc Bäckman <93243973+mabackma@users.noreply.github.com>
2024-07-27 04:31:05 +0300

    todos

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![allow(non_snake_case)]
 
 use dioxus::prelude::*;
-use dioxus_elements::br;
 use dioxus_logger::tracing::{info, Level};
 
 fn main() {    // Init logger
@@ -11,7 +10,7 @@ fn main() {    // Init logger
     dioxus::launch(App);
 }
 
-#[derive(Clone)] // Derive Clone to enable cloning
+#[derive(Clone, Debug)] // Derive Clone to enable cloning
 struct Todo {
     id: i32,
     text: String,
@@ -20,16 +19,15 @@ struct Todo {
 }
 
 impl Todo {
-    // Method to toggle the completed status of a todo
-    fn toggle_completed(&mut self) {
-        self.completed = !self.completed;
+    fn mark_completed(&mut self) {
+        self.completed = true;
     }
 
     // Method to render a todo item
     fn show_todo(&self) -> Element {
         rsx! {
             div {
-                //onclick: move |_| self.toggle_completed(),
+                
                 border: "1px solid black",
                 padding: "10px",
                 margin: "5px",
@@ -53,29 +51,44 @@ fn add_todo(todos: &mut Signal<Vec<Todo>>, todo_name: &Signal<String>, todo_desc
 
     let todo_name: String = todo_name.to_string();
     let description: String = todo_description.to_string();
-    let todo_struct: Todo = Todo {
+    let mut new_todo: Todo = Todo {
         id: todos.len() as i32 + 1,
         text: todo_name.clone(),
         description: description.clone(),
         completed: false,
     };
 
-    todos.push(todo_struct);
+    todos.push(new_todo);
 }
 
 #[component]
 fn App() -> Element {
     let mut todos: Signal<Vec<Todo>> = use_signal(|| Vec::new());
-    let mut todo_name: Signal<String> = use_signal(|| String::from(""));
-    let mut todo_description: Signal<String> = use_signal(|| String::from(""));
+    let mut todo_name: Signal<String> = use_signal(|| "".to_string());
+    let mut todo_description: Signal<String> = use_signal(|| "".to_string());
+
+    // Function to toggle completed state of a todo
+    let mut toggle_todo = move |id: i32| {
+        println!("clicked todo: {}", id);
+        let mut todos = todos.get_mut(id as usize);
+        for todo in todos.iter_mut() {
+            if todo.id == id {
+                todo.completed = !todo.completed;
+            }
+        }
+    };
 
     rsx! {
         link { rel: "stylesheet", href: "main.css" }
         div {
             h1 { "Todos: {todos.len()}" }
+
             div {
                 for todo in todos.iter() {
-                    { todo.show_todo() }
+                    div {
+                        onclick: move |_| toggle_todo(21341234), // toggle_todo(todo.id), FIND A WAY TO PASS THE ID
+                        { todo.show_todo() } // Render the todo
+                    }
                 }
             }
             h3 { "Add a new todo:" }
